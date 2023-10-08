dbf84779a0d7bcccfac3b362390d16b9ca71265e
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-08 20:03:01 +0300

    lesson 2 added

---
+++ lesson2.java
@@ -0,0 +1,8 @@
+class lesson2 {
+    public static void main(String[] args) {
+    System.out.println(3 + 3);
+    System.out.println(2 * 5);
+    System.out.println(10 - 2);
+    System.out.println(10 / 5);
+  }
+}
