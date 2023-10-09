1a0207fc3077b27a6650fbcd0b761cf210171f43
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-09 08:05:25 +0300

    lesson3 added

---
+++ lesson3.java
@@ -0,0 +1,9 @@
+public class lesson3 {
+  public static void main(String[] args) {
+    String name = "Kemal";
+    System.out.println(name);
+    int myNum = 15;
+    System.out.println(myNum);
+    System.out.println(name + " " + myNum);
+  }
+}
