015d99e8c34b4e131533cb4884a13c9520db62ff
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-12 12:45:33 +0300

    lesson 6 added

---
+++ lesson6.java
@@ -0,0 +1,8 @@
+public class lesson6 {
+    public static void main(String[] args){
+        boolean isClicked = true;
+        boolean isUserOnline = false;
+        System.out.println(isClicked);
+        System.out.println(isUserOnline);
+    }
+}
