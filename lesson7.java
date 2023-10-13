4a7450add26d8341f72f4aa8cf33c287490f5904
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-13 13:37:45 +0300

    lesson 7 added

---
+++ lesson7.java
@@ -0,0 +1,9 @@
+public class lesson7 {
+    public static void main(String[] args){
+        char var1 = 65, var2 = 66, var3 = 67;
+        System.out.println(var1 + var2 + var3); // 198
+        System.out.println(var1); // ASCII = A
+        System.out.println(var2); // ASCII = B
+        System.out.println(var3); // ASCII = C
+    }
+}
