e121b4eb162ef14783fd6dffb2fba2da2d73ec15
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-14 15:39:05 +0300

    lesson 8 added

---
+++ lesson8.java
@@ -0,0 +1,16 @@
+public class lesson8 {
+    public static void main(String[] args){
+        
+        int x = 5;
+        double d = x;
+        
+        System.out.println(x); // 5
+        System.out.println(d); // 5.0
+
+        double y = 7.55d;
+        int i = (int) y;
+
+        System.out.println(y); // 7.55
+        System.out.println(i); // 7
+    } 
+}
