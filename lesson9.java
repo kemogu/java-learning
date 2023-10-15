8d9893dad0804f37c8a97dc914621a7a19f551ad
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-15 09:38:38 +0300

    lesson 9 added

---
+++ lesson9.java
@@ -0,0 +1,10 @@
+public class lesson9 {
+    public static void main(String[] args){
+        String myString = "My Name Is Kemogu";
+        System.out.println(myString);
+        System.out.println(myString.length());
+        System.out.println(myString.toLowerCase());
+        System.out.println(myString.indexOf("Name")); // 3
+        System.out.println(myString.indexOf("Is")); // 8
+    }
+}
