ff852ba43ae58e2f111c5377e92716c4052f53be
Kemal Oğuzhan Yeyit <kemaloguzhanyeyit@gmail.com>
2023-10-22 11:21:57 +0300

    lesson 10 added

---
+++ lesson10.java
@@ -0,0 +1,15 @@
+public class lesson10 {
+  public static void main(String[] args) {
+    String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    System.out.println("The length of the txt string is: " + txt.length());
+    
+    String txt2 = "Hello World";
+    System.out.println(txt2.toUpperCase());
+    System.out.println(txt2.toLowerCase());
+  
+    String firstName = "Kemal";
+    String lastName = "Yeyit";
+    System.out.println(firstName + " " + lastName);
+    System.out.println(firstName.concat(lastName));
+}
+}
